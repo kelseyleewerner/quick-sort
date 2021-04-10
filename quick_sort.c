94f52124c5652586b8f3fe8d5a1798d9ae4f8759
kelseyleewerner <kelsey.lee.werner@gmail.com>
2021-04-10 10:35:01 -0700

    io set up and fxn prototype

--- quick_sort.c
+++ quick_sort.c
@@ -2,3 +2,68 @@
 // CS-350
 // 04/15/2021
 // HW #2: Mystery Sort
+
+#include <stdio.h>
+
+
+void mystery_sort(int * arr, int lo, int hi);
+
+
+int main()
+{
+  int i, count, x[1000];
+
+  printf("How many elements are you going to enter?: ");
+  scanf("%d", &count);
+
+  printf("Enter %d elements: ", count);
+  for (i = 0; i < count; ++i)
+    scanf("%d", &x[i]);
+
+  printf("Unsorted Data: ");
+  for (i = 0; i < count; ++i)
+    printf(" %d", x[i]);
+  printf("\n");
+
+  mystery_sort(x, 0, count - 1);
+
+  printf("Sorted Data: ");
+  for (i = 0; i < count; ++i)
+    printf(" %d", x[i]);
+  printf("\n");
+
+  return 0;
+}
+
+
+void mystery_sort(int * arr, int lo, int hi)
+{
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
